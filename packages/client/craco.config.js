1bd30ec6894df5a18a399876345b93949a8ccf51
shivaluma <shivaluma@gmail.com>
2020-12-08 19:19:03 +0700

    Resetup folder of project, add eslint, prettier

--- packages/client/craco.config.js
+++ packages/client/craco.config.js
@@ -1,15 +1,13 @@
-const path = require("path");
-const { getLoader, loaderByName } = require("@craco/craco");
-const absolutePath = path.join(__dirname, "../common");
+/* eslint-disable no-unused-vars */
+const path = require('path');
+const { getLoader, loaderByName } = require('@craco/craco');
+const absolutePath = path.join(__dirname, '../common');
 module.exports = {
   webpack: {
     alias: {},
     plugins: [],
     configure: (webpackConfig, { env, paths }) => {
-      const { isFound, match } = getLoader(
-        webpackConfig,
-        loaderByName("babel-loader")
-      );
+      const { isFound, match } = getLoader(webpackConfig, loaderByName('babel-loader'));
       if (isFound) {
         const include = Array.isArray(match.loader.include)
           ? match.loader.include
---
+++ packages/client/src/components/app/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">alo</div>;
+}
+
+export default App;
