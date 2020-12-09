0efa46e035aac7bc62322e912ac5ea21b715df05
shivaluma <shivaluma@gmail.com>
2020-12-09 08:17:52 +0700

    Fix API

---
+++ packages/common/api.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: `https://shivaluma.wtf/caro/api`,
+});
+
+// localhost eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmOGU5MTM4NzY0OTRlMGE2ZThkNWUxNCIsInVzZXJuYW1lIjoic2hpdmFsdW1hMSIsImRpc3BsYXlOYW1lIjoic2hpdmFsdW1hMSIsImlhdCI6MTYwMzYzNDU1NH0.N8cI-r4LKe5zbVxRyrCBkiTmfVrc9dyoC2aqryMCJeA
+// api       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmOTNiOTA0Y2MwYWJmMTlkMDM5MjU4NyIsInVzZXJuYW1lIjoic2hpdmFsdW1hIiwiZGlzcGxheU5hbWUiOiJzaGl2YWx1bWEiLCJpYXQiOjE2MDM1MTY2ODF9.omnezGwWS6drn4wGFBPjjV6__yheMpZ3B4uJLXyAvb8';
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('whatisthis');
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
