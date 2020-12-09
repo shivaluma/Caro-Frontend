e0356cb334982d36e260b5062ca850b2c5e0d9a8
crepp-tt <tan.thai.2t@gmail.com>
2020-12-10 01:37:29 +0700

    Add purgecss, fix linting, fix socketio endpoint

--- packages/client/src/configs/socket.js
+++ packages/client/src/configs/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'http://35.201.203.222:4000/';
 const socket = io(ENDPOINT, {
   transports: ['websocket'],
   upgrade: false,
