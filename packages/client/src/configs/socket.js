c2423c9713494cc4703c2f6bffa0ff6737b71dfc
shivaluma <shivaluma@gmail.com>
2021-01-14 10:14:19 +0700

    Change url for deploy

--- packages/client/src/configs/socket.js
+++ packages/client/src/configs/socket.js
@@ -1,7 +1,7 @@
 import * as io from 'socket.io-client';
 
 // const ENDPOINT_PROD = 'https://shivaluma.wtf';
-const ENDPOINT_DEV = 'http://localhost:1233';
+const ENDPOINT_DEV = 'https://shivaluma.wtf';
 const socket = io(ENDPOINT_DEV, {
   transports: ['websocket']
 });
